49e09b0408bfdb821b3405ce4798ac18ef0b49d6
Eric_C <a762466074@gmail.com>
2024-07-21 21:11:08 -0400

    Prepare for new branch

--- Utils/Network.js
+++ Utils/Network.js
@@ -3,32 +3,80 @@
 
 //QUERY FOR NETWORK GRAPH TIME CONFINES
 export function fetchNetworkConfines() {
-    if (this.state.node_id === "" && this.state.nodeSelect === "") { return null }
-    else {
+    if (this.state.node_id === "" && this.state.nodeSelect === "") {
+        return null
+    } else {
         let nodeIdFilter;
         fetchNeo4jId(this.state.node_id, this.driver)
             .then((internalId) => {
                 if (this.state.node_id !== "") {
-                    nodeIdFilter = 'WHERE id(n) =' + parseFloat(internalId) + ' '
-                } else { nodeIdFilter = "" };
+                    nodeIdFilter = parseFloat(internalId) + ' '
+                } else {
+                    nodeIdFilter = ""
+                }
                 const session = this.driver.session();
-                const query = `
-        MATCH (n)-[t]-(o)`+ nodeIdFilter + `
-        WITH collect(n.birth_year)+collect(n.death_year)+collect(DISTINCT t.start_year)+collect(DISTINCT t.end_year) as list
-        UNWIND list as years
-        WITH years ORDER BY years
-        WITH (head(collect(distinct years))) as head, (last(collect(distinct years))) as last
-        RETURN {start:head, end:last} as Confines
-        `
+                const query =
+                    ` WITH people_relationships AS (
+                        SELECT n.start_year, n.end_year
+                        FROM people_presentat_institutions n
+                        WHERE n.entity_from_id = ${nodeIdFilter}
+                        UNION ALL
+                        SELECT n.start_year, n.end_year
+                        FROM people_presentat_events n
+                        WHERE n.entity_from_id = ${nodeIdFilter}
+                        UNION ALL
+                        SELECT n.start_year, n.end_year
+                        FROM people_involvedwith_publications n
+                        WHERE n.entity_from_id = ${nodeIdFilter}
+                        UNION ALL
+                        SELECT n.start_year, n.end_year
+                        FROM people_relatedto_people n
+                        WHERE n.entity_from_id = ${nodeIdFilter}
+                        UNION ALL
+                        SELECT n.start_year, n.end_year
+                        FROM people_partof_corporateentities n
+                        WHERE n.entity_from_id = ${nodeIdFilter}
+                        UNION ALL
+                        SELECT n.start_year, n.end_year
+                        FROM people_presentat_generalareas n
+                        WHERE n.entity_from_id = ${nodeIdFilter}
+                    )
+                        SELECT MIN(years) AS start, MAX(years) AS end
+                        FROM (SELECT n.birth_year AS years
+                              FROM people n
+                                       JOIN people_relationships t ON n.id = ${nodeIdFilter}
+                              UNION ALL
+                              SELECT n.death_year AS years
+                              FROM people n
+                                       JOIN people_relationships t ON n.id = ${nodeIdFilter}
+                              UNION ALL
+                              SELECT DISTINCT t.start_year AS years
+                              FROM people_relationships t
+                                       JOIN people n ON n.id = ${nodeIdFilter}
+                              UNION ALL
+                              SELECT DISTINCT t.end_year AS years
+                              FROM people_relationships t
+                                       JOIN people n ON n.id = ${nodeIdFilter}
+                              ) AS YearList
+                              `
+
                 session.run(query).then((results) => {
                     const networkConfines = results.records.map((record) => record.get('Confines'));
                     let start;
                     let end;
-                    if (this.state.start_year === "") { start = networkConfines[0].start } else { start = this.state.start_year }
-                    if (this.state.end_year === "") { end = networkConfines[0].end } else { end = this.state.end_year }
-                    this.setState({ start_year: start })
-                    this.setState({ end_year: end })
-                    this.setState({ time_disable: false })
+                    if (this.state.start_year === "") {
+                        start = networkConfines[0].start
+                    } else {
+                        start = this.state.start_year
+                    }
+                    if (this.state.end_year === "") {
+                        end = networkConfines[0].end
+                    } else {
+                        end = this.state.end_year
+                    }
+                    this.setState({start_year: start})
+                    this.setState({end_year: end})
+                    this.setState({time_disable: false})
                     session.close()
                 });
             })
@@ -41,13 +89,12 @@ export function fetchNetworkConfines() {
 //QUERY FOR NETWORK GRAPH RESULTS
 export function fetchNetworkResults() {
     if (this.state.node_id === "" && this.state.nodeSelect === "") {
-        this.setState({ nosend: "nosend" })
-    }
-    else {
-        this.setState({ content: "loading" });
-        this.setState({ nodeArray: [] });
-        this.setState({ printArray: [] });
-        this.setState({ append: this.state.append + 1 });
+        this.setState({nosend: "nosend"})
+    } else {
+        this.setState({content: "loading"});
+        this.setState({nodeArray: []});
+        this.setState({printArray: []});
+        this.setState({append: this.state.append + 1});
 
         //SET CENTRAL NODE
         let nodeIdFilter;
@@ -55,15 +102,19 @@ export function fetchNetworkResults() {
             .then((internalId) => {
                 if (this.state.node_id !== "") {
                     nodeIdFilter = '' + parseFloat(internalId) + ' '
-                } else { nodeIdFilter = "" };
+                } else {
+                    nodeIdFilter = ""
+                }
+                ;
 
                 //SET NETWORK CONFINES
                 let start;
                 let end;
 
                 const session1 = this.driver.session();
+                // TODO: Code repetition
                 const query2 = `
-          MATCH (n)-[t]-(o) WHERE id(n)=`+ nodeIdFilter + `
+          MATCH (n)-[t]-(o) WHERE id(n)=` + nodeIdFilter + `
           WITH collect(n.birth_year)+collect(n.death_year)+collect(DISTINCT t.start_year)+collect(DISTINCT t.end_year) as list
           UNWIND list as years
           WITH years ORDER BY years
@@ -72,21 +123,60 @@ export function fetchNetworkResults() {
           `
                 session1.run(query2).then((results) => {
                     const networkConfines = results.records.map((record) => record.get('Confines'));
-                    if (this.state.start_year === "") { start = networkConfines[0].start } else { start = this.state.start_year }
-                    if (this.state.end_year === "") { end = networkConfines[0].end } else { end = this.state.end_year }
-                    this.setState({ start_year: start })
-                    this.setState({ end_year: end })
-                    this.setState({ time_disable: false })
+                    if (this.state.start_year === "") {
+                        start = networkConfines[0].start
+                    } else {
+                        start = this.state.start_year
+                    }
+                    if (this.state.end_year === "") {
+                        end = networkConfines[0].end
+                    } else {
+                        end = this.state.end_year
+                    }
+                    this.setState({start_year: start})
+                    this.setState({end_year: end})
+                    this.setState({time_disable: false})
                     session1.close()
                 });
 
                 //CONSTRUCT FILTERS FROM USER INPUT
-                let degreeFilter; if (this.state.degree !== 1) { degreeFilter = this.state.degree } else { degreeFilter = 1 };
-                let peopleFilter; if (this.state.people_include === true) { peopleFilter = "+" } else { peopleFilter = "-" }
-                let instFilter; if (this.state.inst_include === true) { instFilter = "+" } else { instFilter = "-" }
-                let corpFilter; if (this.state.corp_include === true) { corpFilter = "+" } else { corpFilter = "-" }
-                let eventFilter; if (this.state.event_include === true) { eventFilter = "+" } else { eventFilter = "-" }
-                let pubFilter; if (this.state.pub_include === true) { pubFilter = "+" } else { pubFilter = "-" }
+                let degreeFilter;
+                if (this.state.degree !== 1) {
+                    degreeFilter = this.state.degree
+                } else {
+                    degreeFilter = 1
+                }
+                ;
+                let peopleFilter;
+                if (this.state.people_include === true) {
+                    peopleFilter = "+"
+                } else {
+                    peopleFilter = "-"
+                }
+                let instFilter;
+                if (this.state.inst_include === true) {
+                    instFilter = "+"
+                } else {
+                    instFilter = "-"
+                }
+                let corpFilter;
+                if (this.state.corp_include === true) {
+                    corpFilter = "+"
+                } else {
+                    corpFilter = "-"
+                }
+                let eventFilter;
+                if (this.state.event_include === true) {
+                    eventFilter = "+"
+                } else {
+                    eventFilter = "-"
+                }
+                let pubFilter;
+                if (this.state.pub_include === true) {
+                    pubFilter = "+"
+                } else {
+                    pubFilter = "-"
+                }
                 //CONCAT FILTERS
                 const filterStatic = [nodeIdFilter]
                 const filterStaticClean = filterStatic.filter(value => value.length > 1).join();
@@ -96,14 +186,18 @@ export function fetchNetworkResults() {
 
                 let startFilter = null;
                 let endFilter = null;
-                if (this.state.end_year !== "") { endFilter = this.state.end_year }
-                if (this.state.start_year !== "") { startFilter = this.state.start_year }
+                if (this.state.end_year !== "") {
+                    endFilter = this.state.end_year
+                }
+                if (this.state.start_year !== "") {
+                    startFilter = this.state.start_year
+                }
 
                 const query = `MATCH (n)-[t]-(o)
           WHERE id(n) = ` + nodeIdFilter + `
           WITH DISTINCT n, t, o
           CALL apoc.path.subgraphAll(n, {
-            maxLevel: `+ degreeFilter + `,
+            maxLevel: ` + degreeFilter + `,
             labelFilter: "` + peopleFilter + `Person|` + instFilter + `Institution|` + corpFilter + `CorporateEntity|` + eventFilter + `Event|` + pubFilter + `Publication|-Village|-Township|-County|-Prefecture|-Province"
           })
           YIELD nodes, relationships
@@ -116,9 +210,9 @@ export function fetchNetworkResults() {
           )
           WITH collect(DISTINCT ender) AS endNodes
           MATCH (y)
-          WHERE id(y) = `+ nodeIdFilter + `
+          WHERE id(y) = ` + nodeIdFilter + `
           CALL apoc.path.subgraphAll(y, {
-            maxLevel: `+ degreeFilter + `,
+            maxLevel: ` + degreeFilter + `,
             labelFilter: "` + peopleFilter + `Person|` + instFilter + `Institution|` + corpFilter + `CorporateEntity|` + eventFilter + `Event|` + pubFilter + `Publication|-Village|-Township|-County|-Prefecture|-Province",
             blacklistNodes: endNodes
           })
@@ -148,14 +242,17 @@ export function fetchNetworkResults() {
                             ) || (
                                 (this.state.start_year === "" && this.state.end_year === "")
                             )
-                            ) { links.push(ulinks[i]) }
+                            ) {
+                                links.push(ulinks[i])
+                            }
                         }
-                        const nodeArray = [{ nodes: newArray[0].nodes, links: links, }]
-                        if (nodeArray.length === 0) { this.setState({ noresults: "noresults" }) }
-                        else {
-                            this.setState({ nodeArray });
-                            this.setState({ noresults: "noresults hide" })
-                            this.setState({ content: "loaded" })
+                        const nodeArray = [{nodes: newArray[0].nodes, links: links,}]
+                        if (nodeArray.length === 0) {
+                            this.setState({noresults: "noresults"})
+                        } else {
+                            this.setState({nodeArray});
+                            this.setState({noresults: "noresults hide"})
+                            this.setState({content: "loaded"})
                         }
                         session.close()
                     });
